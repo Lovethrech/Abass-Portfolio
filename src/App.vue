876c3caafebd6ec672ffe02fa5e92a68a4fab10d
Lovethrech <94970084+Lovethrech@users.noreply.github.com>
2023-10-20 15:29:07 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import HeaderView from "./view/HeaderView.vue";
+</script>
+
+<template>
+  <video autoplay muted src="../public/TEST RENDER 01 8SEC.mp4"></video>
+  <div class="header">
+    <HeaderView/>
+  </div>
+</template>
+<style scoped>
+@font-face{
+    font-family:"North-well";
+    src: url("./assets/fonts/Northwell/Northwell.ttf");
+}
+video{
+  object-fit:fill;
+  width:100%;
+  position:fixed;
+  top:0;
+  z-index:-1;
+}
+.header{
+  padding: 5px 20px;
+  background-color: hsla(0, 100%, 100%,0.3);
+  border-radius: 0 0 20px 20px;
+}
+</style>
+  
+
---
+++ src/view/HeaderView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="head-icon"><p>Abayomi</p></div>
+        <ul></ul>
+    </div>
+</template>
+
+<style scoped>
+.head-icon{
+    font-size:32px;
+    color: rgb(75, 31, 31);
+}
+p{
+    font-family: North-well;
+    font-weight:500;
+    
+}
+</style>
